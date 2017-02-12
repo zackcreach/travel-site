cebf32cf45abbe5c06bf9bc542da92cea40abf59
Zack Creach <zackcreach@gmail.com>
2017-02-12 11:22:56 -0500

    Fixed jquery syntax error (on vs. load)

--- app/temp/scripts/App.js
+++ app/temp/scripts/App.js
@@ -11283,7 +11283,7 @@ var StickyHeader = function () {
 	_createClass(StickyHeader, [{
 		key: 'refreshWaypoints',
 		value: function refreshWaypoints() {
-			this.lazyImages.load(function () {
+			this.lazyImages.on("load", function () {
 				Waypoint.refreshAll();
 			});
 		}
